c2c20bc90c23a5e90f683e94e0417bf747cb05f6
BinaryQuasar <quasar@dworld.io>
2017-12-10 13:58:25 +0100

    Set supportsInterface to pure.

--- contracts/DWorldToken.sol
+++ contracts/DWorldToken.sol
@@ -16,7 +16,7 @@ contract DWorldToken is DWorldBase, ERC721 {
     /// @notice Introspection interface as per ERC-165 (https://github.com/ethereum/EIPs/issues/165).
     /// Returns true for any standardized interfaces implemented by this contract.
     /// (ERC-165 and ERC-721.)
-    function supportsInterface(bytes4 _interfaceID) external view returns (bool) {
+    function supportsInterface(bytes4 _interfaceID) external pure returns (bool) {
         return ((_interfaceID == INTERFACE_SIGNATURE_ERC165) || (_interfaceID == INTERFACE_SIGNATURE_ERC721));
     }
     
