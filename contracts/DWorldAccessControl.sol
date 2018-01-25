70db0a4a0ef0b4f7f9431dd6e8609b6e9473b854
BinaryQuasar <quasar@dworld.io>
2018-01-25 01:25:55 +0100

    Improve inline documentation.

--- contracts/DWorldAccessControl.sol
+++ contracts/DWorldAccessControl.sol
@@ -9,18 +9,18 @@ contract DWorldAccessControl is Claimable, Pausable, CanReclaimToken {
     address public cfoAddress;
 
     function DWorldAccessControl() public {
-        // the creator of the contract is the initial CFO
+        // The creator of the contract is the initial CFO.
         cfoAddress = msg.sender;
     }
     
-    /// @dev Access modifier for CFO-only functionality
+    /// @dev Access modifier for CFO-only functionality.
     modifier onlyCFO() {
         require(msg.sender == cfoAddress);
         _;
     }
 
     /// @dev Assigns a new address to act as the CFO. Only available to the current CEO.
-    /// @param _newCFO The address of the new CFO
+    /// @param _newCFO The address of the new CFO.
     function setCFO(address _newCFO) external onlyOwner {
         require(_newCFO != address(0));
 
