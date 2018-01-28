ec2f864e1cbe77c092ebacbd2d01443df28074a7
BinaryQuasar <quasar@dworld.io>
2018-01-28 15:03:34 +0100

    Add mainnet configuration.

--- truffle.js
+++ truffle.js
@@ -10,6 +10,14 @@ module.exports = {
       network_id: '4',
       gasPrice: 2500000000, // 2.5 gwei
     },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(config.mnemonic, config.mainnetProvider);
+      },
+      network_id: '1',
+      gas: 4295748,
+      gasPrice: 1500000000, // 1.5 gwei
+    },
   },
   solc: {
     optimizer: {
