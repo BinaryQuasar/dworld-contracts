aa88bec719229b83e2f2a7fa30e0af5d18118aa8
BinaryQuasar <quasar@dworld.io>
2018-02-03 13:47:47 +0100

    Update deployment scripts.

--- migrations/2_core.js
+++ migrations/2_core.js
@@ -13,12 +13,12 @@ module.exports = async function(deployer, network) {
             originalContractAddress = "0xf8c2b13ACcf82c66B01160a3Aaaa8f9299EFcfd4";
             originalSaleAuctionAddress = "0x7990F0A9286D46aB4Ced513522cC1E23A22b3489";
             originalRentAuctionAddress = "0x9F2EfDbAb961dA25FE39D44f9f6fC95fba32A71b";
-            buyoutsEnabledAfterHours = 1;
+            buyoutsEnabledAfterHours = 2;
         } else if (network == "mainnet") {
             originalContractAddress = "0xd4Df33983FF82CE4469c6ea3CFf390403E58d90A";
             originalSaleAuctionAddress = "0x621aD3562F5141c4A0E7Cad958b8b524d356332B";
             originalRentAuctionAddress = "0x5301F1EC2F48F86bbd5291DfD7998a3d733A3245";
-            buyoutsEnabledAfterHours = 24;
+            buyoutsEnabledAfterHours = 15;
         }
 
         return DWorldCore.new(
